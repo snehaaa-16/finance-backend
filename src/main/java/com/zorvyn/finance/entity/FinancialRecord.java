bf4684b43e2c4fb9ba7b6a94eaf97615086eb390
snehaaa-16 <snehajaiswal1607@gmail.com>
2026-04-03 09:22:38 +0530

    addded entities

---
+++ src/main/java/com/zorvyn/finance/entity/FinancialRecord.java
@@ -0,0 +1,44 @@
+package com.zorvyn.finance.entity;
+
+import com.zorvyn.finance.enums.Category;
+import com.zorvyn.finance.enums.RecordType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "financial_records")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FinancialRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private RecordType type;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    private LocalDate date;
+
+    private String notes;
+
+    // Soft delete
+    private LocalDateTime deletedAt;
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+}
