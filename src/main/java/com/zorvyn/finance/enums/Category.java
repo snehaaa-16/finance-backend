3cbaa856f4b58d74e2a806c9d2f8691e9a6c56b6
snehaaa-16 <snehajaiswal1607@gmail.com>
2026-04-03 09:20:10 +0530

    addded enums

---
+++ src/main/java/com/zorvyn/finance/enums/Category.java
@@ -0,0 +1,19 @@
+package com.zorvyn.finance.enums;
+
+public enum Category {
+    SALARY,
+    FREELANCE,
+    INVESTMENT,
+    RENT,
+    UTILITIES,
+    FOOD,
+    TRANSPORT,
+    HEALTHCARE,
+    EDUCATION,
+    ENTERTAINMENT,
+    SHOPPING,
+    EMI,
+    TAX,
+    INSURANCE,
+    OTHER
+}
---
+++ src/main/java/com/zorvyn/finance/enums/Role.java
@@ -0,0 +1,7 @@
+package com.zorvyn.finance.enums;
+
+public enum Role {
+    ROLE_VIEWER,
+    ROLE_ANALYST,
+    ROLE_ADMIN
+}
